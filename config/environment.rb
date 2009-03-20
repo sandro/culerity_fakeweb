7cab66977056e955b1714b2d7381cc555b685daa
Sandro Turriate <sandro.turriate@gmail.com>
2009-03-20 17:59:15 -0400

    Adding gems to config

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'rspec-rails', :lib => 'spec', :version => '>= 1.1.11'
+  config.gem 'haml'
+  config.gem 'giraffesoft-resource_controller', :source => 'http://gems.github.com', :lib => 'resource_controller'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
